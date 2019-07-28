3cedcb479b35950024f196380f8c972eb14becdc
taosang1992 <taosang1992@gmail.com>
2019-07-28 17:15:08 +0800

    增加React相关配置依赖

--- src/search.js
+++ src/search.js
@@ -1,3 +1,18 @@
-()=>{
-    console.log('arrow function');
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Search extends React.Component{
+    render(){
+        return <h1>Some Search Text</h1>;
+    }
+
+
 }
+
+ReactDOM.render(
+    <Search /> ,
+    document.getElementById('root')
+)
+
