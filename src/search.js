791cf7440813a09506da850264434ef4114db17a
taosang1992 <taosang1992@gmail.com>
2019-07-29 08:30:12 +0800

    增加watch相关配置

--- src/search.js
+++ src/search.js
@@ -9,7 +9,7 @@ class Search extends React.Component{
     render(){
         return <h1 className="some-text">
             <img src={logo}/>
-            <span>Some Search Text</span>
+            <span>Search Text</span>
             </h1>;
     }
 }
